c9f8536909e23d4aede9c97c656899f201014a4b
Katharine Jiang <k.jiang@berkeley.edu>
2019-06-14 18:21:27 -0700

    add sticky headers to nextdoor page

--- assets/scripts/script_projects.js
+++ assets/scripts/script_projects.js
@@ -2,4 +2,22 @@ var backToStart = document.getElementsByClassName("back-to-start")[0];
 
 backToStart.addEventListener("click", function() {
     window.location.href = "index.html?fromProjectPage=1";
-})
+});
+
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var header = document.getElementById("myHeader");
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
