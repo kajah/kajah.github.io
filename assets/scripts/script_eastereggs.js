79d1ee02e268859a50355cda0ffef97c122f45db
Katharine Jiang <k.jiang@berkeley.edu>
2019-06-20 12:51:54 -0700

    finished shoes modal

--- assets/scripts/script_eastereggs.js
+++ assets/scripts/script_eastereggs.js
@@ -1,8 +1,28 @@
-var musicEmbed  = document.getElementById("music-embed"),
-    music       = document.getElementById("music");
+var musicEmbed      = document.getElementById("music-embed"),
+    music           = document.getElementById("music")
+    musicVisible    = false,
+    modalShoe       = document.getElementsByClassName("modal")[0],
+    shoes           = document.getElementById("shoes"),
+    modalClose      = document.getElementsByClassName("modal-close")[0];
 
 musicEmbed.style.visibility = "hidden";
 
 music.addEventListener("click", function() {
-    musicEmbed.style.visibility = "visible";
+    if (!musicVisible) {
+        musicEmbed.style.visibility = "visible";
+        musicVisible = true;
+    } else {
+        musicEmbed.style.visibility = "hidden";
+        musicVisible = false;
+    }
+    
 });
+
+shoes.addEventListener("click", function() {
+    modalShoe.style.visibility = "visible";
+});
+
+modalClose.addEventListener("click", function() {
+    modalShoe.style.visibility = "hidden";
+});
+
