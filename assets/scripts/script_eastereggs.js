6810cd25d74bd3a9f7e3a651ecb1f6e17a1d2821
Katharine Jiang <k.jiang@berkeley.edu>
2019-06-21 01:03:58 -0700

    added kanaad easter egg

--- assets/scripts/script_eastereggs.js
+++ assets/scripts/script_eastereggs.js
@@ -3,9 +3,14 @@ var musicEmbed      = document.getElementById("music-embed"),
     musicVisible    = false,
     modalShoe       = document.getElementsByClassName("modal")[0],
     shoes           = document.getElementById("shoes"),
-    modalClose      = document.getElementsByClassName("modal-close")[0];
+    modalClose      = document.getElementsByClassName("modal-close")[0],
+    kanaad          = document.getElementById("kanaad"),
+    kanaadPic       = document.getElementById("kanaad-pic");
+    and             = document.getElementById("and");
 
 musicEmbed.style.visibility = "hidden";
+kanaad.style.visibility = "hidden";
+kanaadPic.style.visibility = "hidden";
 
 music.addEventListener("click", function() {
     if (!musicVisible) {
@@ -25,4 +30,20 @@ shoes.addEventListener("click", function() {
 modalClose.addEventListener("click", function() {
     modalShoe.style.visibility = "hidden";
 });
+
+and.addEventListener("click", function() {
+    kanaad.style.visibility = "visible";
+});
+
+kanaad.addEventListener("click", function() {
+    kanaadPic.style.visibility = "visible";
+});
+
+kanaadPic.addEventListener("click", function() {
+    kanaadPic.style.visibility = "hidden";
+    kanaad.style.visibility = "hidden";
+});
+
+
+
 
