b54b2d5a4ce8f742950a5298bb2d8f89568772fa
Katharine Jiang <k.jiang@berkeley.edu>
2019-06-24 10:51:30 -0700

    cleaned up some files

--- assets/scripts/script_projects.js
+++ assets/scripts/script_projects.js
@@ -7,32 +7,3 @@ backToStart.addEventListener("click", function() {
 window.addEventListener('popstate', function() {
     window.location.href = "index.html?fromProjectPage=1";
 });
-
-// document.addEventListener("keypress", keyEvents);
-
-// function keyEvents(event) {
-//     var x = event.key;
-
-//     if (x == "a") {
-//         alert ("You pressed the 'backspace' key!");
-//     }
-// }
-
-
-// // When the user scrolls the page, execute myFunction 
-// window.onscroll = function() {myFunction()};
-
-// // Get the header
-// var header = document.getElementById("myHeader");
-
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
