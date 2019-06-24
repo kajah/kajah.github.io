77787a138b04a768c6ca895129ad348d7c954221
Katharine Jiang <k.jiang@berkeley.edu>
2019-06-23 17:22:40 -0700

    added hello world easter egg

--- assets/scripts/script_eastereggs.js
+++ assets/scripts/script_eastereggs.js
@@ -6,11 +6,17 @@ var musicEmbed      = document.getElementById("music-embed"),
     modalClose      = document.getElementsByClassName("modal-close")[0],
     kanaad          = document.getElementById("kanaad"),
     kanaadPic       = document.getElementById("kanaad-pic");
-    and             = document.getElementById("and");
+    and             = document.getElementById("and"),
+    hi              = document.getElementById("hi"),
+    originalText    = document.getElementById("typedtext"),
+    newText         = document.getElementById("typewriter-text");
+
+var numClicksHi = 0;
 
 musicEmbed.style.visibility = "hidden";
 kanaad.style.visibility = "hidden";
 kanaadPic.style.visibility = "hidden";
+newText.style.visibility = "hidden";
 
 music.addEventListener("click", function() {
     if (!musicVisible) {
@@ -42,6 +48,37 @@ kanaadPic.addEventListener("click", function() {
     kanaadPic.style.visibility = "hidden";
     kanaad.style.visibility = "hidden";
 });
+
+hi.addEventListener("click", function() {
+    numClicksHi += 1;
+    if (numClicksHi > 2) {
+        originalText.style.visibility = "hidden";
+        newText.style.visibility = "visible";
+        newText.innerHTML = "Hello World! Did you find the other easter eggs?";
+    }
+});
+
+newText.addEventListener("click", function() {
+    originalText.style.visibility = "visible";
+    newText.innerHTML = "";
+    newText.style.visibility = "hidden";
+    numClicksHi = 0;
+});
+
+
+document.addEventListener("keypress", keyEvents);
+
+function keyEvents(event) {
+    var key = event.key;
+
+    if (key == "a") {
+        alert ("You pressed the 'backspace' key!");
+    }
+}
+
+
+
+
 
 
 
