72048d3ad1012d2092879afbceeff47ede3f0e57
Katharine Jiang <katharine.jiang9@gmail.com>
2020-12-29 14:56:05 -0500

    clean up dir

--- assets/scripts/script_projects.js
+++
@@ -1,9 +0,0 @@
-var backToStart = document.getElementsByClassName("back-to-start")[0];
-
-backToStart.addEventListener("click", function() {
-    window.location.href = "index.html?fromProjectPage=1";
-});
-
-window.addEventListener('popstate', function() {
-    window.location.href = "index.html?fromProjectPage=1";
-});
