86453e8e52eb73a5ca6cd8d41df8f00e0eebe8c3
Katharine Jiang <katharine.jiang9@gmail.com>
2020-12-29 20:09:03 -0500

    add audio and photos

--- assets/scripts/script_2020.js
+++ assets/scripts/script_2020.js
@@ -2,11 +2,16 @@ var description     = document.querySelector("#description-content"),
     photo           = document.querySelector("#character-photo"),
     nextButton      = document.querySelector("#next-button"),
     prevButton      = document.querySelector("#prev-button"),
+    backButton      = document.querySelector("#back"),
     health          = document.querySelector("#health");
 
-var photos = ['./assets/images/2019/graduation.jpg', './assets/images/2019/vegas.jpeg']
+var photos = ['./assets/images/2020/maine-min.jpg', './assets/images/2020/1jan.jpeg', './assets/images/2020/2feb1-min.jpg', './assets/images/2020/2feb2-min.jpg', './assets/images/2020/3march.jpg', './assets/images/2020/4april-min.jpg', './assets/images/2020/5may-min.jpg', './assets/images/2020/6june-min.jpg', './assets/images/2020/7july-min.jpg', './assets/images/2020/8august-min.jpg', './assets/images/2020/9sept-min.jpg', './assets/images/2020/10oct-min.JPG','./assets/images/2020/fire-min.JPG', './assets/images/2020/11nov-min.jpg', './assets/images/2020/12dec-min.JPG'];
 var currPhotoIndex = 0;
 
+
+back.addEventListener("click", function() {
+  window.location.href = "index.html"
+})
 
 health.addEventListener("click", function() {
   description.innerText = "Hello world";
@@ -17,7 +22,7 @@ nextButton.addEventListener("click", function() {
   if (currPhotoIndex > photos.length - 1) {
     currPhotoIndex = 0;
   }
-  photo.src = photos[currPhotoIndex]
+  photo.src = photos[currPhotoIndex];
 })
 
 prevButton.addEventListener("click", function() {
