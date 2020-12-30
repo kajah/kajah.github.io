2f273374bfd67d3bc4950266b00a937b2610e496
Katharine Jiang <katharine.jiang9@gmail.com>
2020-12-30 12:58:37 -0500

    add wellness copy

--- assets/scripts/script_2020.js
+++ assets/scripts/script_2020.js
@@ -1,4 +1,5 @@
 var description     = document.querySelector("#description-content"),
+    descriptionBox  = document.querySelector(".description-content-container"),
     photo           = document.querySelector("#character-photo"),
     nextButton      = document.querySelector("#next-button"),
     prevButton      = document.querySelector("#prev-button"),
@@ -14,7 +15,8 @@ back.addEventListener("click", function() {
 })
 
 health.addEventListener("click", function() {
-  description.innerText = "Hello world";
+  // description.innerText = "Hello world";
+  descriptionBox.innerHTML = '\<p\>This was a rough one. I\'ve never experienced the levels of stress \& anxiety as I did this year. Like so many others, I struggled with the unknown of the world, loneliness, and feeling of hopelessness when waking up and going to bed. I stopped working out and my sudden stationary lifestyle took a toll on my physical and mental health. I had difficulty falling asleep for the first time ever, breathing issues, and ongoing headaches stemmed from stress, overwork, and a concussion in March. I cried a lot.\</p\>\<p\>I still feel the effects of the sheer weight from stress that bore down on me for so many months. But I found solace in candles \& aromatherapy, yoga, reading, and art. I have put down multiple chapters of my life, freeing brain space and allowing me to focus on my next big chapter. This year was my wake up call, as my body urgently alerted me to pay attention to mental wellness. Stress is unavoidable, but we can be aware when we\'ve crossed a line, and take a breather to reset.\</p\>'
 })
 
 nextButton.addEventListener("click", function() {
